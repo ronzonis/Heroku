aae3377c7292220150a623629411f03d01965f49
ronzonis <33371394+ronzonis@users.noreply.github.com>
2017-12-06 15:25:37 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -29,7 +29,7 @@ $resArray = json_decode($content,true);
 //print_r($resArray['response']['venues']);
 foreach($resArray['response']['venues'] as $key => $value)
 {    
-    echo "Luogo $key: ".$value['name'].", situato in ".$value['location']['address']."<br>";
+    echo "Luogo $key: ".$value['name'].", latitudine: ".$value['location']['lat'].", longitudine: ".$value['location']['lng']."<br>";
     
 }
 
