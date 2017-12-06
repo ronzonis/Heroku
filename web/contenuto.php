cd8262d96825958c626bfea8ccdeda100f68425b
ronzonis <33371394+ronzonis@users.noreply.github.com>
2017-12-06 15:55:29 +0100

    Update contenuto.php
    Aggiunto l'header

--- web/contenuto.php
+++ web/contenuto.php
@@ -31,6 +31,39 @@ echo '<head><link rel="stylesheet" href="https://fonts.googleapis.com/icon?famil
 <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script></head>';
 
+?>
+<body>
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+          <header class="mdl-layout__header">
+            <div class="mdl-layout__header-row">
+              <!-- Title -->
+              <span class="mdl-layout-title" id='titolo'>Pannello di controllo</span>
+              <!-- Add spacer, to align navigation to the right -->
+              <div class="mdl-layout-spacer"></div>
+              <!-- Navigation. We hide it in small screens. -->
+              <nav class="mdl-navigation mdl-layout--large-screen-only">
+                <div class="mdl-navigation__link" onclick="passa_a(1);" style="cursor:pointer" id="home_btn"><i class="material-icons">home</i></div>
+                        <div class="mdl-tooltip mdl-tooltip--bottom" data-mdl-for="home_btn">
+                                        Home
+                                </div>
+                <div class="mdl-navigation__link" onclick="passa_a(-2);"  style="cursor:pointer"><i class="material-icons" id="logout_btn">exit_to_app</i></div>
+                        <div class="mdl-tooltip mdl-tooltip--bottom" data-mdl-for="logout_btn">
+                                        Logout
+                                </div>
+                <!--<a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>-->
+              </nav>
+            </div>
+        </header>
+        <main class="mdl-layout__content">
+        <div class="page-content" >
+
+
+
+<?
+
+
 
 echo "<table class='mdl-data-table mdl-js-data-table mdl-shadow--2dp'>";
 echo "<thead>";
@@ -48,5 +81,5 @@ foreach($resArray['response']['venues'] as $key => $value)
     
 }
 echo "</table>";
-
+echo "</div></body>";
 ?>
