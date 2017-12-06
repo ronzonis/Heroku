a6404f9424e62f2bd92cf315f8e52b0a1127056f
ronzonis <33371394+ronzonis@users.noreply.github.com>
2017-12-06 14:04:45 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,6 +2,26 @@
 echo("Mostra pizzerie in Bergamo");
 //require('../vendor/autoload.php');
 
+
+/ create curl resource 
+$ch = curl_init(); 
+
+// set url 
+curl_setopt($ch, CURLOPT_URL, "https://api.foursquare.com/v2/venues/search?v=20161016&near=bergamo&query=pizzeria&intent=checkin&client_id=2BQU5RNR31YWW5UXL0UQ3BGDT04ZRZOOLH5Z454K5NJOMFNX&client_secret=2YKLYMCCYNYTIW4JLD3TJYE2LDB2YHRBKXDQR45GK5PYOCCB"); 
+
+//return the transfer as a string 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+// $output contains the output string 
+$output = curl_exec($ch); 
+
+// close curl resource to free up system resources 
+curl_close($ch);  
+
+echo "<br><br>".$output;
+
+
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
