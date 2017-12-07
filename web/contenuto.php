692b69977cf431b64aacf7d8f64524b910a6a8bb
ronzonis <33371394+ronzonis@users.noreply.github.com>
2017-12-07 14:29:03 +0100

    Update contenuto.php

--- web/contenuto.php
+++ web/contenuto.php
@@ -54,12 +54,12 @@ echo '<head><meta name="viewport" content="width=device-width, initial-scale=1.0
 }        
 </style>
 
-<body onload='dimensiona();'>
+<body>
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
           <header class="mdl-layout__header">
             <div class="mdl-layout__header-row">
               <!-- Title -->
-              <span class="mdl-layout-title" id='titolo'>Pannello di controllo</span>
+              <span class="mdl-layout-title" id='titolo'>Ricerca luoghi</span>
               <!-- Add spacer, to align navigation to the right -->
               <div class="mdl-layout-spacer"></div>
               <!-- Navigation. We hide it in small screens. -->
@@ -103,11 +103,6 @@ echo '<head><meta name="viewport" content="width=device-width, initial-scale=1.0
 </style>         
                 
 <script>                
-function dimensiona()
-{
-	document.getElementById("card").style.width = parseInt(document.getElementById('tabella').style.width)+"px;";
-	console.log(document.getElementById('tabella').width);
-}
 	
 function controlla()
 {
